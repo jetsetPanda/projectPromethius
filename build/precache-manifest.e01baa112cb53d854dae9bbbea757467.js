8573eb6e4da3a95d737a20e90b362b3f62400b0a
jetsetPanda <marionong@gmail.com>
2020-01-12 20:05:28 -0500

    deployed app on firebase

---
+++ build/precache-manifest.e01baa112cb53d854dae9bbbea757467.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f5118a4034ab8dcc2e8b4196e18568",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3e177756604095dc4a1",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "057d2271984d67180c2d",
+    "url": "/static/js/2.80547a2f.chunk.js"
+  },
+  {
+    "revision": "3e98c76dbeabfc2237e3ff9322117f97",
+    "url": "/static/js/2.80547a2f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3e177756604095dc4a1",
+    "url": "/static/js/main.e7fdac72.chunk.js"
+  },
+  {
+    "revision": "7b31455c1cc4caec37f8",
+    "url": "/static/js/runtime-main.0351c1b7.js"
+  }
+]);
